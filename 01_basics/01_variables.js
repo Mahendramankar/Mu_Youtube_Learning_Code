a3bbbfdc0de510766ff088733c48919392d4f373
Mahendramankar <50709280+Mahendramankar@users.noreply.github.com>
2024-09-28 15:43:36 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountID = 12345
+let accountEmail = "mahendra@google.com"
+var accountPassword = "123123"
+accountCity = "Maharashtra"
+
+accountEmail = "mankar@google.com"
+accountPassword = "56789"
+accountCity = "Akola"
+console.table([accountID,accountEmail,accountPassword,accountCity])
